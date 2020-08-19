953406002c0c9bf04449539ccdc10dc44869482d
Bandiap <paulsbandy@gmail.com>
2020-08-20 00:54:56 +0500

    scripts added (all checked previously)

---
+++ public/javascripts/end.js
@@ -0,0 +1,8 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+finalScore.innerText = mostRecentScore;
