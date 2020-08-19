65ee16b90d9725213db208cd4549d7c674c7ec4c
Bandiap <paulsbandy@gmail.com>
2020-08-20 00:55:20 +0500

    routes corrected

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,14 +2,10 @@ module.exports.home = (req, res) => {
   res.render('index')
 }
 
-module.exports.game = (req, res) => {
-  res.render('game')
+module.exports.play = (req, res) => {
+  res.render('play',{ data: req.body })
 }
 
 module.exports.end = (req, res) => {
   res.render('end')
-}
-
-module.exports.play = (req, res) => {
-  res.render('play',{ data: req.body })
 }
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const router = express.Router()
 // Home
 const mainCtrl = require('../controllers/homeController.js')
 router.get('/', mainCtrl.home)
-router.get('/game', mainCtrl.game)
-router.get('/end', mainCtrl.end)
 router.post('/play', mainCtrl.play)
+router.get('/end', mainCtrl.end)
 
 router.use((request, response)=>{
   response.status('404').send('404, page not found');
